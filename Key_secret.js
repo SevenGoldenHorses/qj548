9b563d4125bbad2c3f562b402c95cfe742af5b33
3d4K9ZlAUUic <33258239+SevenGoldenHorses@users.noreply.github.com>
2018-05-12 18:18:30 +0200

    token fix

--- Key_secret.js
+++ Key_secret.js
@@ -1,8 +1,8 @@
 module.exports = {
   consumer_key:         'lSplur12fy7fNRCPVL76IwRQh',
   consumer_secret:      'k1m7MV9a0ARpcS7EDsiH0uC6BFLpf3f8tDQJXfdJli7tmzETtm',
-  access_token:         '726776221-nT8mMl9i0QKpd0dVdBlaN592U9klADjf1ojKPdBj',
-  access_token_secret:  '6uwExu8Ggd3Ej5nCR0BHHv00aSISg8Wtz8QfqIXcm9UJf',
+  access_token:         '726776221-Wl4ymvd21MqBToG9zqUFsqs3tFt6XwVraAIsDfYZ',
+  access_token_secret:  'yr4AnSdajRF180im4hHVqREiSC72ZvM2ucpB2u8PkBLEb',
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
 
